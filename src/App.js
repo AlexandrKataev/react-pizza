90660225814ddf9b5194a69985ea9141c026e128
AlexandrKataev <dkeyman@mail.ru>
2022-09-23 16:24:28 +0300

    scss + hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
+import './scss/app.scss';
+import Header from './components/Header/Header';
+import Categories from './components/Categories/Categories';
+import Sort from './components/Sort/Sort';
+import PizzaBlock from './components/PizzaBlock/PizzaBlock';
+
 function App() {
-  const test = () => {
-    return "тест jsx";
-  };
   return (
-    <div className="App">
-      <h1>Hello</h1>
-      <div>{test()}</div>
+    <div>
+      <div className="wrapper">
+        <Header />
+
+        <div className="content">
+          <div className="container">
+            <div className="content__top">
+              <Categories />
+              <Sort />
+            </div>
+            <h2 className="content__title">Все пиццы</h2>
+            <div className="content__items">
+              <PizzaBlock title="Мясная" price="679" />
+              <PizzaBlock title="Пепперони" price="499" />
+              <PizzaBlock title="Мексиканская" price="599" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
