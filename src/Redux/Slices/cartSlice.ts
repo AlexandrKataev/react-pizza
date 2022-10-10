0bf6e0e7d5eb2a71d6a18c13b93e1299ae82f59d
AlexandrKataev <dkeyman@mail.ru>
2022-10-10 22:22:37 +0300

    TS (async, extraReducers, Action)

---
+++ src/Redux/Slices/cartSlice.ts
@@ -0,0 +1,79 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { RootState } from '../store';
+
+export type CartItem = {
+  id: string;
+  title: string;
+  price: number;
+  imageUrl: string;
+  size: number;
+  type: string;
+  count: number;
+};
+
+interface CartState {
+  totalPrice: number;
+  items: CartItem[];
+}
+
+const initialState: CartState = {
+  totalPrice: 0,
+  items: [],
+};
+
+export const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    // addItem: (state, action) => {
+    //   state.items.push(action.payload);
+    //   state.totalPrice = state.items.reduce((sum, obj) => {
+    //     return obj.price + sum;
+    //   }, 0);
+    // },
+    addItem: (state, action: PayloadAction<CartItem>) => {
+      const findItem = state.items.find((obj) => obj.id === action.payload.id);
+      if (findItem) {
+        findItem.count++;
+      } else {
+        state.items.push({ ...action.payload, count: 1 });
+      }
+      state.totalPrice = state.items.reduce((sum, obj) => {
+        return obj.price * obj.count + sum;
+      }, 0);
+    },
+
+    minusItem: (state, action: PayloadAction<string>) => {
+      const findItem = state.items.find((obj) => obj.id === action.payload);
+      if (findItem) {
+        findItem.count--;
+      }
+      state.totalPrice = state.items.reduce((sum, obj) => {
+        return obj.price * obj.count + sum;
+      }, 0);
+    },
+
+    removeItem: (state, action: PayloadAction<string>) => {
+      state.items = state.items.filter((obj) => obj.id != action.payload);
+      state.totalPrice = state.items.reduce((sum, obj) => {
+        return obj.price * obj.count + sum;
+      }, 0);
+    },
+    clearItem: (state) => {
+      state.items = [];
+      state.totalPrice = state.items.reduce((sum, obj) => {
+        return obj.price * obj.count + sum;
+      }, 0);
+    },
+  },
+});
+
+export const selectCartState = (state: RootState) => state.cart;
+export const selectCart = (state: RootState) => state.cart.items;
+export const selectPizza = (id: string) => (state: RootState) =>
+  state.cart.items.find((obj) => obj.id === id);
+export const selectCartTotalPrice = (state: RootState) => state.cart.totalPrice;
+
+export const { addItem, removeItem, clearItem, minusItem } = cartSlice.actions;
+
+export default cartSlice.reducer;
